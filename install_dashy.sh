55ed16b7889c6eefae43c6eebca095ad9ba51872
Damn <51714144+damndemento@users.noreply.github.com>
2023-05-15 12:27:47 +0200

    Create install_dashy.sh

---
+++ install_dashy.sh
@@ -0,0 +1,44 @@
+# first update the system
+apt update && apt full-upgrade
+# install required packages.
+apt install git curl net-tools
+# install node.js version 16.x
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo bash -
+apt update
+apt install -y nodejs
+apt update && apt upgrade -y
+# remove cmdtest
+apt remove cmdtest
+apt autoremove
+# prepare yarn installation
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+apt update
+apt install yarn
+# download dashy
+cd /opt/
+git clone https://github.com/Lissy93/dashy.git
+cd /opt/dashy/
+# install dashy dependencies
+yarn
+# set the max amount of memory used for building dashy
+export NODE_OPTIONS=--max-old-space-size=1000
+# build dashy
+yarn build
+# create a system service for dashy
+cat <<EOF > /etc/systemd/system/dashy.service
+[Unit]
+Description=Dashy
+After=network.target
+[Service]
+Type=simple
+ExecStart=/usr/bin/yarn --cwd /opt/dashy/ start
+[Install]
+WantedBy=multi-user.target
+Alias=Dashy
+EOF
+# enable the service for systemctl
+systemctl enable dashy
+systemctl start dashy
+echo congratulations, script finished without errors!
+echo open your browser with <container-ip>:4000 to configure dashy...
